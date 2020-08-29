87bbc60c3140c6600a0e8707ccefa03d0e192018
Lucas Ethan <asmrit1010@gmail.com>
2020-08-29 08:27:59 +0200

    Made code even say if number is odd or even

--- numbers.rb
+++ numbers.rb
@@ -1,14 +1,24 @@
 #!/usr/bin/env ruby
-
+# Asks user for a integer
 print "Enter a integer: "
-integer = gets.to_f
-if
-  "#{integer.to_f}" < "0"
+integer = gets.to_i
+# If integer is negative
+if 
+  "#{integer}" < "0"
   puts "#{integer} is a negative number"
 end
-
+# If integer is positive
 if
-  "#{integer.to_f}" > "0"
-  puts "#{integer} is a positive number"
+ "#{integer}" > "0"
+   puts "#{integer} is a positive number" 
 end
-
+# If integer is odd
+if
+  integer.odd? == true
+  puts "#{integer} is an odd number"
+end
+# If integer is even
+if
+  integer.even? == true
+  puts "#{integer} is an even number"
+end
