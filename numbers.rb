487c8d12d251e850c98d2c26ef72cc7567d71549
Lucas Ethan <asmrit1010@gmail.com>
2020-08-30 18:18:00 +0200

    Made code say if number is composite and fixed bugs

--- numbers.rb
+++ numbers.rb
@@ -8,11 +8,18 @@ if
   integer.positive? == true
   puts "#{integer} is a positive number"
 end
+
 # If integer is positive
 if
    integer.negative? == true
    puts "#{integer} is a negative number" 
 end
+
+# if the integer is 0
+if integer == 0
+    puts "#{integer} is neither positive or negative"
+end
+
 # If integer is odd
 if
   integer.odd? == true
@@ -21,10 +28,16 @@ end
 # If integer is even
 if
   integer.even? 
-  puts "#{integer} is an even number"
+  puts "#{integer} is an even number" unless integer == 0
 end
 # If integer is prime
 if
   integer.abs.prime? == true
   puts "#{integer} is a prime number"
+end
+
+# If integer is composite
+if 
+  integer.abs.prime? == false
+  puts "#{integer} is a composite number" unless integer == 1
 end
