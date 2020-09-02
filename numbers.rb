c29f2cb9efbffba0fe24bbb84cc3899aa390d083
Lucas Ethan <asmrit1010@gmail.com>
2020-09-02 09:58:16 +0200

    Made code say if you entered a invalid input

--- numbers.rb
+++ numbers.rb
@@ -3,27 +3,32 @@
 require "prime"
 
 # Asks user for a integer
-print "Enter a integer: "
-integer = gets.to_i
+print "Enter an integer: "
+integer = Integer(gets, exception: false)
 
-if integer.positive? 
-  puts "#{integer} is a positive number"
-elsif integer.negative?
-  puts "#{integer} is a negative number" 
+if integer
+  if integer.positive? 
+    puts "#{integer} is a positive number"
+  elsif integer.negative?
+    puts "#{integer} is a negative number" 
+  else
+    puts "#{integer} is neither positive or negative"
+  end
+
+  if integer.odd?
+    puts "#{integer} is an odd number"
+  else
+    puts "#{integer} is an even number"
+  end
+
+  if integer.abs.prime?
+    puts "#{integer} is a prime number"
+  elsif integer.zero? || integer == 1
+    puts "#{integer} is neither prime nor composite"
+  else
+    puts "#{integer} is a composite number"
+  end
 else
-  puts "#{integer} is neither positive or negative"
+  puts "Invalid Input"
 end
 
-if integer.odd?
-  puts "#{integer} is an odd number"
-else
-  puts "#{integer} is an even number"
-end
-
-if integer.abs.prime?
-  puts "#{integer} is a prime number"
-elsif integer.zero? || integer == 1
-  puts "#{integer} is neither prime nor composite"
-else
-  puts "#{integer} is a composite number"
-end
